a20ef792fc2b7e708b3a1ee0acbdbabbfba73be6
MatthiasHeylenTM2021 <r0468021@student.thomasmore.be>
2024-02-08 15:31:56 +0100

    added routes & middleware

---
+++ hello-world-web/cmd/web/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"myapp/pkg/config"
+	"myapp/pkg/handlers"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	// mux := pat.New()
+
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
--- hello-world-web/go.mod
+++ hello-world-web/go.mod
@@ -1,3 +1,8 @@
 module myapp
 
 go 1.22.0
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
