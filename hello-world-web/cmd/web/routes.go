975821ee0e34d7dd536a838d71bd2848a99f689f
MatthiasHeylenTM2021 <r0468021@student.thomasmore.be>
2024-02-08 15:51:53 +0100

    experimenting with sessions

--- hello-world-web/cmd/web/routes.go
+++ hello-world-web/cmd/web/routes.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -16,7 +17,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux := chi.NewRouter()
 
+	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
