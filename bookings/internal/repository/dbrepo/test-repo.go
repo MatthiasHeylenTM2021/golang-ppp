7887a18edd252db74ce21b61428f16e88b924833
MatthiasHeylenTM2021 <r0468021@student.thomasmore.be>
2024-02-11 12:09:50 +0100

    added all & new, and reseration details to admin dashboard

--- bookings/internal/repository/dbrepo/test-repo.go
+++ bookings/internal/repository/dbrepo/test-repo.go
@@ -67,3 +67,24 @@ func (m *testDBRepo) AllNewReservations() ([]models.Reservation, error) {
 
 	return reservations, nil
 }
+
+func (m *testDBRepo) GetReservationByID(id int) (models.Reservation, error) {
+
+	var res models.Reservation
+
+	return res, nil
+}
+
+func (m *testDBRepo) UpdateReservation(u models.Reservation) error {
+
+	return nil
+}
+
+func (m *testDBRepo) DeleteReservation(id int) error {
+
+	return nil
+}
+
+func (m *testDBRepo) UpdateProcessedForReservation(id, processed int) error {
+	return nil
+}
